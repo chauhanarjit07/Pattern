319363d404896560f8aa0824cfba4bddc399629e
chauhanarjit07 <chauhanarjit07@gmail.com>
2026-06-10 23:13:46 +0530

    Update Diamond_Star_Pattern.cpp

--- Diamond_Star_Pattern.cpp
+++ Diamond_Star_Pattern.cpp
@@ -1,1 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int i,j,n,k;
+  cout<<"Enter rows: ";
+    cin>> n;
+  for(i=0; i<n; i++){
+    for(j=0; j<n-i-1; j++){
+      cout<<" ";
+    }
+    for(k=0; k<2*(i+1)-1; k++){
+        cout<<"*";
+      }
+    cout<<endl;
+  }
+  for(i=0; i<n; i++){
+    for(j=0; j<i; j++){
+      cout<<" ";
+    }
+    for(k=0; k<(2*n)-1-(2*i); k++){
+        cout<<"*";
+      }
+    cout<<endl;
+  }
+  return 0;
+}
+
+
+  
+  
 
